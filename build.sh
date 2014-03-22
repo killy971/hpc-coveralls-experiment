6662ec92cb47c86148539f4d8afaf1147051aedf
killy971 <killy971@gmail.com>
2014-03-22 16:18:52 +0900

    Added Travis CI config and coveralls.io script with upload disabled

---
+++ build.sh
@@ -0,0 +1,3 @@
+JSON="{\"service_name\":\"travis-ci\", \"service_job_id\":\"$TRAVIS_JOB_ID\", \"source_files\":[{\"coverage\":[null,0,1,2],\"name\":\"file1.hs\",\"source\":\"line1\nline2\nline3\nline4\"}]}"
+echo $JSON > coverage.json
+#curl -F json_file=@coverage.json https://coveralls.io/api/v1/jobs
