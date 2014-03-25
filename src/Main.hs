3f2bd60c6315991e38b78e751e646c72dddbc229
killy971 <killy971@gmail.com>
2014-03-25 22:10:57 +0900

    Added debug trace

--- src/Main.hs
+++ src/Main.hs
@@ -10,7 +10,7 @@ postJson url jsonData = do
     setopt curl (CurlPost True)
     setopt curl (CurlPostFields [jsonData])
     setopt curl (CurlHttpHeaders ["Content-Type: application/json"])
-    response <- perform_with_response curl
+    response <- perform_with_response_ curl
     reset curl
     return response
 
@@ -20,6 +20,8 @@ main = do
     case args of
         [jobId] -> do
             let jsonData = "{\"service_name\":\"travis-ci\", \"service_job_id\":\"" ++ jobId ++ "\", \"source_files\":[{\"coverage\":[null,0,1,2],\"name\":\"file1.hs\"}]}"
-            _ <- postJson "https://coveralls.io/api/v1/jobs" jsonData
+            response <- postJson "https://coveralls.io/api/v1/jobs" jsonData
+            putStrLn jsonData
+            putStrLn $ show $ respStatus response
             return ()
         _ -> error "illegal arguments"
