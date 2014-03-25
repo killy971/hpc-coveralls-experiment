51c04312280346cfdcf1782e5c3c211bdff1ec51
killy971 <killy971@gmail.com>
2014-03-25 21:53:31 +0900

    Experiment to use libcurl haskell binding

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-JSON="{\"service_name\":\"travis-ci\", \"service_job_id\":\"$TRAVIS_JOB_ID\", \"source_files\":[{\"coverage\":[null,0,1,2],\"name\":\"file1.rb\",\"source\":\"line1\nline2\nline3\nline4\"}, {\"coverage\":[null,0,1,2],\"name\":\"file1.hs\",\"source\":\"line1\nline2\nline3\nline4\"}, {\"coverage\":[null,0,1,2],\"name\":\"file1.ml\",\"source\":\"line1\nline2\nline3\nline4\"}, {\"coverage\":[null,0,1,2],\"name\":\"file1.clj\",\"source\":\"line1\nline2\nline3\nline4\"}]}"
-echo $JSON > coverage.json
-curl -F json_file=@coverage.json https://coveralls.io/api/v1/jobs
+#JSON="{\"service_name\":\"travis-ci\", \"service_job_id\":\"$TRAVIS_JOB_ID\", \"source_files\":[{\"coverage\":[null,0,1,2],\"name\":\"file1.rb\",\"source\":\"line1\nline2\nline3\nline4\"}, {\"coverage\":[null,0,1,2],\"name\":\"file1.hs\",\"source\":\"line1\nline2\nline3\nline4\"}, {\"coverage\":[null,0,1,2],\"name\":\"file1.ml\",\"source\":\"line1\nline2\nline3\nline4\"}, {\"coverage\":[null,0,1,2],\"name\":\"file1.clj\",\"source\":\"line1\nline2\nline3\nline4\"}]}"
+#echo $JSON > coverage.json
+#curl -F json_file=@coverage.json https://coveralls.io/api/v1/jobs
+cabal run $TRAVIS_JOB_ID
---
+++ src/Main.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import Network.Curl
+import System.Environment (getArgs)
+
+postJson :: String -> String -> IO CurlResponse
+postJson url jsonData = do
+    curl <- initialize
+    setopt curl (CurlURL url)
+    setopt curl (CurlPost True)
+    setopt curl (CurlPostFields [jsonData])
+    setopt curl (CurlHttpHeaders ["Content-Type: application/json"])
+    response <- perform_with_response curl
+    reset curl
+    return response
+
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+        [jobId] -> do
+            let jsonData = "{\"service_name\":\"travis-ci\", \"service_job_id\":\"" ++ jobId ++ "\", \"source_files\":[{\"coverage\":[null,0,1,2],\"name\":\"file1.hs\"}]}"
+            _ <- postJson "https://coveralls.io/api/v1/jobs" jsonData
+            return ()
+        _ -> error "illegal arguments"
