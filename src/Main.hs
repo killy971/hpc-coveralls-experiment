5873e913a7cbac79bb06a0324a4f783eb456a82c
killy971 <killy971@gmail.com>
2014-03-26 22:42:59 +0900

    Fixed newline escape

--- src/Main.hs
+++ src/Main.hs
@@ -32,7 +32,7 @@ main = do
     args <- getArgs
     case args of
         [jobId] -> do
-            let jsonData = "{\"service_name\":\"travis-ci\", \"service_job_id\":\"" ++ jobId ++ "\", \"source_files\":[{\"coverage\":[null,0,1,2],\"name\":\"file1.hs\",\"source\":\"line1\\\nline2\\\nline3\\\nline4\"}]}"
+            let jsonData = "{\"service_name\":\"travis-ci\", \"service_job_id\":\"" ++ jobId ++ "\", \"source_files\":[{\"coverage\":[null,0,1,2],\"name\":\"file1.hs\",\"source\":\"line1\\nline2\\nline3\\nline4\"}]}"
             response <- postJson "https://coveralls.io/api/v1/jobs" jsonData
             putStrLn jsonData
             putStrLn $ show $ respStatus response
